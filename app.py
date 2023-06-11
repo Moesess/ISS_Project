5155ca7bc0e259d89c3497d66faee9e687e1a2bc
Tomasz Kaszuba <tomek@kaszuba.eu>
2023-06-12 00:01:47 +0200

    TK fuzzy control

--- app.py
+++ app.py
@@ -54,58 +54,17 @@ for i in range(1, len(t)):
     q_in, e, e_int = pid_controller(t[i], h[i - 1], h_ref, h[i - 2] if i > 1 else h[i - 1], e_int, dt)
     h[i] = odeint(tank, h[i - 1], [t[i - 1], t[i]], args=(q_in,))[1]
 
-# Regulator rozmyty
-# Definiowanie zbiorów rozmytych
-e = ctrl.Antecedent(np.arange(-1, 12, 0.01), 'e')  # zwiększony zakres
-de = ctrl.Antecedent(np.arange(-1, 10, 0.01), 'de')  # zwiększony zakres
-q_in = ctrl.Consequent(np.arange(-1, 12, 0.01), 'q_in')  # zwiększony zakres
 
-e.automf(7)  # Więcej funkcji przynależności
-de.automf(7)  # Więcej funkcji przynależności
-q_in.automf(7)  # Więcej funkcji przynależności
-
-# Zdefiniuj więcej reguł dla większej liczby funkcji przynależności
-rule1 = ctrl.Rule(e['poor'] & de['poor'], q_in['poor'])
-rule2 = ctrl.Rule(e['mediocre'] & de['mediocre'], q_in['mediocre'])
-rule3 = ctrl.Rule(e['average'] & de['average'], q_in['average'])
-rule4 = ctrl.Rule(e['decent'] & de['decent'], q_in['decent'])
-rule5 = ctrl.Rule(e['good'] & de['good'], q_in['good'])
-rule6 = ctrl.Rule(e['excellent'] & de['excellent'], q_in['excellent'])
-rule7 = ctrl.Rule(e['dismal'] & de['dismal'], q_in['dismal'])
-
-control_system = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7])
-controller = ctrl.ControlSystemSimulation(control_system)
 
 h0 = 0.0
 h = np.zeros_like(t)
 h[0] = h0
 e_prev = 0.0
 
-for i in range(1, len(t)):
-    e = h_ref - h[i - 1]
-    de = (e - e_prev) / dt
-    controller.input['e'] = e
-    controller.input['de'] = de
-    controller.compute()
-    q_in = controller.output['q_in']
-    h[i] = odeint(tank, h[i - 1], [t[i - 1], t[i]], args=(q_in,))[1]
-    e_prev = e
-
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.H1('Symulacja systemu sterowania zbiornikiem'),
-
-    html.Div([
-        html.Label('Wpływ wody'),
-        dcc.Slider(
-            id='q_in-slider',
-            min=0,
-            max=10,
-            step=0.1,
-            value=1.5
-        )
-    ]),
 
     html.Div([
         html.Label('Zadany poziom wody'),
@@ -184,10 +143,43 @@ app.layout = html.Div([
         )
     ]),
 
+    html.Div([
+        html.Label('e'),
+        dcc.Slider(
+            id='e-slider',
+            min=5,
+            max=20,
+            step=0.5,
+            value=10
+        )
+    ]),
+
+    html.Div([
+        html.Label('de'),
+        dcc.Slider(
+            id='de-slider',
+            min=5,
+            max=20,
+            step=0.5,
+            value=10
+        )
+    ]),
+
+    html.Div([
+        html.Label('Współczynnik wpływu wody'),
+        dcc.Slider(
+            id='q_in-slider',
+            min=5,
+            max=20,
+            step=0.5,
+            value=10
+        )
+    ]),
+
     html.Div(children=[
-        dcc.Graph(id='graph1', style={'display': 'inline-block', 'width': '60vh'}),
-        dcc.Graph(id='graph2', style={'display': 'inline-block', 'width': '60vh'}),
-        dcc.Graph(id='graph3', style={'display': 'inline-block', 'width': '60vh'})
+        dcc.Graph(id='graph1', style={'display': 'inline-block', 'width': '55vh'}),
+        dcc.Graph(id='graph2', style={'display': 'inline-block', 'width': '55vh'}),
+        dcc.Graph(id='graph3', style={'display': 'inline-block', 'width': '55vh'})
     ])
 ])
 
@@ -196,7 +188,7 @@ app.layout = html.Div([
     [Output('graph1', 'figure'),
      Output('graph2', 'figure'),
      Output('graph3', 'figure')],
-    [Input('q_in-slider', 'value'),
+    [
      Input('h_ref-slider', 'value'),
      Input('t-slider', 'value'),
      Input('p-slider', 'value'),
@@ -204,12 +196,38 @@ app.layout = html.Div([
      Input('d-slider', 'value'),
      Input('h-slider', 'value'),
      Input('a-slider', 'value'),
+     Input('e-slider', 'value'),
+     Input('de-slider', 'value'),
+     Input('q_in-slider', 'value'),
      ]
 )
-def update_graph(q_in_slider_value, h_ref_slider_value, t_value, p_value, i_value, d_value, h_value, a_value):
+def update_graph(h_ref_slider_value, t_value, p_value,
+                 i_value, d_value, h_value, a_value, e_slider, de_slider, q_in_slider):
+    # Regulator rozmyty
+    # Definiowanie zbiorów rozmytych
+    e = ctrl.Antecedent(np.arange(-1, e_slider, 0.01), 'e')  # zwiększony zakres
+    de = ctrl.Antecedent(np.arange(-1, de_slider, 0.01), 'de')  # zwiększony zakres
+    q_in_ctr = ctrl.Consequent(np.arange(-1, q_in_slider, 0.01), 'q_in')  # zwiększony zakres
+
+    e.automf(7)  # Więcej funkcji przynależności
+    de.automf(7)  # Więcej funkcji przynależności
+    q_in_ctr.automf(7)  # Więcej funkcji przynależności
+
+    # Zdefiniuj więcej reguł dla większej liczby funkcji przynależności
+    rule1 = ctrl.Rule(e['poor'] & de['poor'], q_in_ctr['poor'])
+    rule2 = ctrl.Rule(e['mediocre'] & de['mediocre'], q_in_ctr['mediocre'])
+    rule3 = ctrl.Rule(e['average'] & de['average'], q_in_ctr['average'])
+    rule4 = ctrl.Rule(e['decent'] & de['decent'], q_in_ctr['decent'])
+    rule5 = ctrl.Rule(e['good'] & de['good'], q_in_ctr['good'])
+    rule6 = ctrl.Rule(e['excellent'] & de['excellent'], q_in_ctr['excellent'])
+    rule7 = ctrl.Rule(e['dismal'] & de['dismal'], q_in_ctr['dismal'])
+
+    control_system = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7])
+    controller = ctrl.ControlSystemSimulation(control_system)
+
     # Symulacja zbiornika bez regulatora
     t = np.linspace(0, t_value)
-    q_in = q_in_slider_value  # Użyj wartości z suwaka
+    q_in = q_in_slider  # Użyj wartości z suwaka
     h0 = h_value
     h = odeint(tank, h0, t, args=(q_in, a_value,))
     trace1 = go.Scatter(x=t, y=np.squeeze(h), mode='lines', name='Zbiornik bez regulatora')
